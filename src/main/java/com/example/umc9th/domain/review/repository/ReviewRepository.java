3025383313be627bf03b221969a6f8a2bf4ed08a
Hyejun Koo <khjcoco3387@ewhain.net>
2025-11-02 22:46:15 +0900

    feat: ch05 JPQL 미션

--- src/main/java/com/example/umc9th/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/umc9th/domain/review/repository/ReviewRepository.java
@@ -1,10 +1,27 @@
 package com.example.umc9th.domain.review.repository;
 
 import com.example.umc9th.domain.review.entity.Review;
-import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
     Optional<Review> findByUserMission_Id(Long userMissionId);
-    boolean existsByUserMission_Id(Long userMissionId); // 한 이력당 리뷰 1개 검증용
+    boolean existsByUserMission_Id(Long userMissionId);
+
+    // ✅ 가게 상세 - 리뷰 목록 (JPQL + 페이징)
+    @Query("""
+        SELECT r
+        FROM Review r
+        JOIN FETCH r.user u
+        WHERE r.store.id = :storeId
+        ORDER BY r.id DESC
+    """)
+    List<Review> findByStoreIdOrderByIdDesc(@Param("storeId") Long storeId, Pageable pageable);
 }
